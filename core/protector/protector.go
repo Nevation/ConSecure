9caf1fd998b626c400ab91a3c2b97814025b48b4
Nevation <j209446@naver.com>
2023-11-18 07:03:57 +0000

    feat: protector

--- core/protector/protector.go
+++ core/protector/protector.go
@@ -1,1 +1,18 @@
 package protector
+
+import (
+	"consecure/constant"
+	"consecure/util/log"
+	"consecure/util/process"
+)
+
+type Protector struct{}
+
+func NewProtector() *Protector {
+	return &Protector{}
+}
+
+func (p *Protector) Protect(event *constant.EngineEvent) {
+	process.KillProcess(event.Event.Pid)
+	log.Info("Protector kill process", event.Event.Pid)
+}
